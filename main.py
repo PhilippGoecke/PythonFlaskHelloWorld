fb5e0a11a8939b3376e6739c2d141505d6e2ab8c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-03-26 18:08:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index(name):
-  if not name:
-    name = 'World'
+@app.route('/<name>')
+def index(name='World'):
   return 'Hello ' + str(name) + '!'
 
-app.run(host='0.0.0.0', port=8001)
+app.run(host='0.0.0.0', port=5000)
