26e5b038ccc756502620d0e4a8d0dca1bd2fdbbc
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-03-26 18:07:34 +0100

    Update podman-run-flask.bash

--- podman-run-flask.bash
+++ podman-run-flask.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t flask:demo .
-podman run --interactive --tty -p 8001:8001 flask:demo
+podman run --interactive --tty -p 8001:5000 flask:demo
 echo "browse http://localhost:8001/"
