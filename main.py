c0d909630a8d5f8b63639092572c5fd358df00d3
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-03-26 18:14:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<name>')
-def index(name='World'):
+def index():
+  name = request.args.get('name', default = 'World', type = str)
   return 'Hello ' + str(name) + '!'
 
 app.run(host='0.0.0.0', port=5000)
