4a1c59f95a2299b26628515f5e2a1042d7b7c683
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-08 23:19:17 +0100

    Add Flask version to greeting response
    Include Flask version in the greeting response.

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
   name = request.args.get('name', default = 'World', type = str)
-  return 'Hello ' + str(name) + '!'
+  import flask
+  return 'Hello ' + str(name) + '!<br>(Flask version: ' + flask.__version__ + ')'
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
