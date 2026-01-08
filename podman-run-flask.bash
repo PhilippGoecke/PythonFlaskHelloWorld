51e15ff7592e2d8b802e45f3faf508d6faf23425
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-08 23:26:41 +0100

    Update echo message with query parameter

--- podman-run-flask.bash
+++ podman-run-flask.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t flask:demo .
 podman run --interactive --tty -p 8001:5000 flask:demo
-echo "browse http://localhost:8001/"
+echo "browse http://localhost:8001/?name=Test"
